c50bac9da2f8aee9d2e9fc94ff0285f9d78d8197
jiabinC <1402021538@qq.com>
2018-05-22 14:10:39 +0800

    dd

--- src/main/java/com/sale/myweb/controller/CommoditController.java
+++ src/main/java/com/sale/myweb/controller/CommoditController.java
@@ -3,6 +3,8 @@ package com.sale.myweb.controller;
 import com.sale.myweb.services.ClothService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,13 @@ public class CommoditController {
     @Autowired
     ClothService clothService;
 
+    @GetMapping("/index")
+    public String index(ModelMap model) {
+
+        model.addAttribute("allCloths",clothService.getAllCloth());
+
+        return "index";
+    }
     @GetMapping("/cloths")
     public List getCloth() {
         return clothService.findByDiscount("30","60");
