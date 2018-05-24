d4f7baa0c7f9f6bf248c2bdd6022c43e90b48333
jiabinC <1402021538@qq.com>
2018-05-24 21:32:03 +0800

    abin

--- src/main/java/com/sale/myweb/controller/CartsController.java
+++ src/main/java/com/sale/myweb/controller/CartsController.java
@@ -1,4 +1,35 @@
 package com.sale.myweb.controller;
 
+
+import com.sale.myweb.entity.Carts;
+import com.sale.myweb.entity.Users;
+import com.sale.myweb.services.CartsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
 public class CartsController {
+
+    private final CartsService cartsService;
+
+    @Autowired
+    public CartsController(CartsService cartsService) {
+        this.cartsService = cartsService;
+    }
+
+    @PostMapping("addClothToCart")
+    public String addClothToCart(@RequestParam String clothId, @RequestParam String clothNumber, HttpSession session, ModelMap model) {
+        Users users = (Users) session.getAttribute("user");
+        cartsService.addClothsToCarts(new Carts(users.getUserId(),clothId,clothNumber));
+        List<Carts> carts = cartsService.getMyCarts(users);
+        model.addAttribute("carts",carts);
+        model.addAttribute("users",users);
+        return "myCart";
+    }
 }
--- src/main/java/com/sale/myweb/controller/OtherController.java
+++ src/main/java/com/sale/myweb/controller/OtherController.java
@@ -1,22 +1,29 @@
 package com.sale.myweb.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OtherController {
 
-    @GetMapping("/sendEmail")
+    @Autowired
+    JavaMailSender mailSender;
+
+    @PostMapping("/sendEmail")
     public String sendEmail(@RequestParam("mailTitle") String mailTitle, @RequestParam("mailContent") String mailContent, ModelMap model) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setFrom("abin_c@sina.com");
         simpleMailMessage.setTo("abin_c@sina.com");
         simpleMailMessage.setSubject(mailTitle);
         simpleMailMessage.setText(mailContent);
+        mailSender.send(simpleMailMessage);
         model.addAttribute("result","邮件已发送");
         return "contact";
 
