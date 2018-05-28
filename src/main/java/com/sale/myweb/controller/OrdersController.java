3fffe3b3d6890998ade361d119b2cc6217307872
jiabinC <abin_c@sina.com>
2018-05-28 10:38:01 +0800

    msy

--- src/main/java/com/sale/myweb/controller/OrdersController.java
+++ src/main/java/com/sale/myweb/controller/OrdersController.java
@@ -43,7 +43,7 @@ public class OrdersController {
         double totalPrice=0 ;
         for (int i=0;i<split.length;i++) {
 
-          totalPrice = totalPrice + Double.parseDouble(split[i].split(",")[0]) * Double.parseDouble(split[i].split(",")[1])*Double.parseDouble(clothService.findByclothId(split[i].split(",")[0]).getClothDiscount())*0.01;
+          totalPrice = totalPrice + Double.parseDouble(clothService.findByclothId(split[i].split(",")[0]).getClothPrice()) * Double.parseDouble(split[i].split(",")[1])*Double.parseDouble(clothService.findByclothId(split[i].split(",")[0]).getClothDiscount())*0.01;
 
         }
         Orders orders = new Orders(users.getUserAddress(),"审核中",users.getUserTel(),"待交付",users.getUserId(),String.valueOf(totalPrice));
